9cf6d62b60633b0ab44c28a264d713268e8a7a79
not0k <dev@ehret.at>
2023-04-23 16:26:53 +0200

    add start and stop command

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import os
+import logging
+from typing import Final
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dotenv import load_dotenv
+from telegram import Update, Message, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, CallbackContext
+
+import db
+
+load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+TELEGRAM_TOKEN: Final = os.getenv('TELEGRAM_TOKEN')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# Commands
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id: int = update.effective_user.id
+
+    entry = db.select_user(user_id)
+    if entry is not None:
+        await update.message.reply_text(
+            'You are already receiving notifications!\n'
+            'If you want a command list, tap the menu button below.'
+        )
+        return
+
+    await update.message.reply_text('Send me your location or live location to get started!')
 
 
-# Press the green button in the gutter to run the script.
+async def stop_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id: int = update.effective_user.id
+
+    entry = db.select_user(user_id)
+    if entry is None:
+        await update.message.reply_text(
+            'You are not receiving notifications!\n'
+            'Send me your location to start receiving notifications...'
+        )
+        return
+
+    db.delete_user(user_id)
+    await update.message.reply_text(
+        'You will no longer receive notifications!\n'
+        'Send /start to start receiving notifications again.'
+    )
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Create the application
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Command handlers
+    app.add_handler(CommandHandler('start', start_command))
+
+    # Start the application
+    app.run_polling()
